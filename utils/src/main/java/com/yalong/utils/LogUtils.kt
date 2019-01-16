b3131a5dfc9c791b9c9cb39d85d7d8728a78987a
yalong0521 <10295010@qq.com>
2019-01-16 18:40:18 +0800

    Add LogUtils switch

--- utils/src/main/java/com/yalong/utils/LogUtils.kt
+++ utils/src/main/java/com/yalong/utils/LogUtils.kt
@@ -4,7 +4,11 @@ import android.util.Log
 import java.util.*
 
 object LogUtils {
-    private val isDebug = BuildConfig.DEBUG
+    private var mSwitch = true
+
+    fun switch(status: Boolean) {
+        mSwitch = status
+    }
 
     private fun generateTag(): String {
         val caller = Throwable().stackTrace[2]
@@ -16,98 +20,98 @@ object LogUtils {
     }
 
     fun logD(content: String) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.d(tag, content)
     }
 
     fun logD(content: String, tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.d(tag, content, tr)
     }
 
     fun logE(content: String) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.e(tag, content)
     }
 
     fun logE(content: String, tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.e(tag, content, tr)
     }
 
     fun logI(content: String) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.i(tag, content)
     }
 
     fun logI(content: String, tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.i(tag, content, tr)
     }
 
     fun logV(content: String) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.v(tag, content)
     }
 
     fun logV(content: String, tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.v(tag, content, tr)
     }
 
     fun logW(content: String) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.w(tag, content)
     }
 
     fun logW(content: String, tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.w(tag, content, tr)
     }
 
     fun logW(tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.w(tag, tr)
     }
 
     fun logWTF(content: String) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.wtf(tag, content)
     }
 
     fun logWTF(content: String, tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.wtf(tag, content, tr)
     }
 
     fun logWTF(tr: Throwable) {
-        if (!isDebug) return
+        if (!mSwitch) return
         val tag = generateTag()
 
         Log.wtf(tag, tr)
